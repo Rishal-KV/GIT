9909a7eabb5a8e57a839514e77c48786e2a73941
Rishal-KV <rishkv923@gmail.com>
2024-06-04 18:50:52 +0530

    new-branch

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const userRoute = require('./routes/userRoute')
 
 
+app.use('/', userRoute);
 app.listen(3000,()=>{
     console.log("server is running ")
 })
