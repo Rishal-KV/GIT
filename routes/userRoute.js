57c8604e2eae8a17ca4f8d93aca37bbcfcb78ef7
Rishal-KV <rishkv923@gmail.com>
2024-06-04 19:04:34 +0530

    new-branch

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const userRoute = express.Router();
-
+const userDetails = {
+    name : "rishal",
+    age: "20"
+}
+userRoute.get('/',(req,res) => {
+    res.json(userDetails);
+})
 
 module.exports = userRoute
